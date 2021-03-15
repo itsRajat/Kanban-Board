dcef41d7ae6a188ae2d97b618672a583c9ee8489
rajatplease <itsrajatkaushik@gmail.com>
2021-03-16 02:44:05 +0530

    fixed onBlur firing on child elements

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -43,7 +43,19 @@ const Input = ({ setOpen, listId, type, open }) => {
     }
   };
 
-  const handleBlur = (e) => {};
+  const handleBlur = (e) => {
+    const currentTarget = e.currentTarget;
+
+    // Check the newly focused element in the next tick of the event loop
+    setTimeout(() => {
+      // Check if the new activeElement is a child of the original container
+      if (!currentTarget.contains(document.activeElement)) {
+        setTitle('');
+        setCardDescription('');
+        setOpen(false);
+      }
+    }, 0);
+  };
 
   return (
     <div onBlur={handleBlur}>
